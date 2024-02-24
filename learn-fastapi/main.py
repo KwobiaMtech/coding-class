d365e156d11c83fc46e8576b6933850df0a5e713
Kwabena Oduro <patrick@payro.io>
2024-02-24 11:44:48 +0000

    created items body request

--- learn-fastapi/main.py
+++ learn-fastapi/main.py
@@ -1,20 +1,24 @@
-from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
-@app.get("/name")
-def my_name():
-    return {"name": "Sai"}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
